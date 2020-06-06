608e890c82e0e41658e6a4be8c117833d403ec90
Arlene Sanchez <arlene.sanchez004@gmail.com>
2020-06-06 13:30:11 -0400

    fixed schema syntax errors, added seed.sql, added connection.js

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INT NOT NULL AUTO INCREMENT PRIMARY KEY;
-    burger_name VARCHAR(30);
-    devoured BOOL DEFAULT FALSE;
-)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(30),
+    devoured BOOL DEFAULT FALSE
+);
---
+++ db/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("hamburger", FALSE), ("cheeseburger", FALSE), ("veggieburger", FALSE);
