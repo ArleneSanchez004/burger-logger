e7ed033612ff4a0ce6a65e48e11df5869494f596
Arlene Sanchez <arlene.sanchez004@gmail.com>
2020-06-06 17:42:34 -0400

    handlebars views created

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: loacalhost,
+    host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
